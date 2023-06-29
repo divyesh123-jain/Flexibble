90c34deb1b7ce53cdb9940ea1606f9a31088c163
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2023-06-29 12:03:16 +0530

    footer work started

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import Image from 'next/image'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexStart footer'>
+
+        <div className='flex flex-col gap-12 w-full'>
+            <div className='flex items-start flex-col'>
+
+                <Image 
+                src="/logo-purple.svg"
+                width={115}
+                height={38}
+                alt="flexibble"
+                />
+
+                <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    Tempore amet dolores dolorum corrupti porro nesciunt iste nihil ex deserunt ea earum unde suscipit rerum nam ipsam aliquam, 
+                    voluptatum vitae veritatis?
+                </p>
+
+            </div>
+        </div>
+
+    </footer>
   )
 }
 
